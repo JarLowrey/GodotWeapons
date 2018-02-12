a401e2dba11cf7111bf3bdce5d27d8044713d499
James Lowrey <jarlowrey@gmail.com>
2018-02-12 09:13:01 -0500

    improved bullet kill conditions

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -4,22 +4,17 @@ onready var my_muzzle = get_node("../..")
 onready var my_gun = get_node("../../../..")
 
 #death/kill/free() related vars
-export var kill_after_time = false
+export var kill_after_time = false setget set_kill_after_time
 export var kill_on_exit = true setget set_kill_on_viewport_exit
 export var kill_after_travel_dist = -1
 
 var traveled_dist = 0 #must be updated in 2d/3d children physics_process
 
-var _vis_signal_is_connected = false
-
 signal bullet_killed
 
 func _ready():
-	if kill_after_time:
-		$SelfDestructTimer.connect("timeout",self,"kill")
-		$SelfDestructTimer.start()
-	
 	set_kill_on_viewport_exit(kill_on_exit) #setting from editor will not connect signal since node is not loaded
+	set_kill_after_time(kill_after_time) #setting from editor will not connect signal since node is not loaded
 
 func _physics_process(delta):
 	if kill_after_travel_dist > 0 and traveled_dist >= kill_after_travel_dist:
@@ -28,14 +23,25 @@ func _physics_process(delta):
 func set_kill_on_viewport_exit(val):
 	kill_on_exit = val
 	if is_inside_tree():
-		if kill_on_exit and not _vis_signal_is_connected:
-			$VisibilityNotifier.connect("screen_exited",self,"kill")
-			_vis_signal_is_connected = true
-		elif _vis_signal_is_connected:
+		var is_cntd = $VisibilityNotifier.is_connected("screen_exited",self,"kill")
+		if val:
+			if not is_cntd:
+				$VisibilityNotifier.connect("screen_exited",self,"kill")
+		elif is_cntd:
 			$VisibilityNotifier.disconnect("screen_exited",self,"kill")
-			_vis_signal_is_connected = false
+
+func set_kill_after_time(val):
+	kill_after_time = val
+	if is_inside_tree():
+		var is_cntd = $SelfDestructTimer.is_connected("timeout",self,"kill")
+		if val:
+			if not is_cntd:
+				$SelfDestructTimer.connect("timeout",self,"kill")
+				$SelfDestructTimer.start()
+		elif is_cntd:
+			$SelfDestructTimer.disconnect("timeout",self,"kill")
+			$SelfDestructTimer.stop()
 
 func kill(arg=null):
-	print("killed")
 	emit_signal("bullet_killed",self)
 	queue_free()
