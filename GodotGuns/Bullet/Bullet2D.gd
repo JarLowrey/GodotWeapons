6c7e97fc3579cd73add32f031ad9e6a477a76899
James Lowrey <jarlowrey@gmail.com>
2018-04-02 16:21:29 -0700

    fixes

--- GodotGuns/Bullet/Bullet2D.gd
+++ GodotGuns/Bullet/Bullet2D.gd
@@ -3,13 +3,13 @@ extends 'res://GodotGuns/Bullet/Bullet.gd'
 #private vars
 onready var _prev_pos = self.global_position
 var speed = 1500
-var _vel = Vector2()
+var velocity = Vector2()
 
 func fired():
 	self.z_index = my_gun.z_index - 1
 	self.global_rotation = my_muzzle.global_rotation
 	self.global_position = my_muzzle.global_position
-	_vel = Vector2(speed,0).rotated(self.global_rotation)
+	velocity = Vector2(speed,0).rotated(self.global_rotation)
 
 func _physics_process(delta):
 	move(delta)
@@ -20,4 +20,4 @@ func _physics_process(delta):
 		_prev_pos = self.global_position
 
 func move(delta): #override if you want different movement type
-	self.global_position += _vel * delta
+	self.global_position += velocity * delta
